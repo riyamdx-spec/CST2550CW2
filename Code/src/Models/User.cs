e6c4f2a1b4563dbf6e932a6881094dc8bb28a875
Megane Coret <m01014266.mdx@gmail.com>
2026-03-01 18:16:47 +0400

    add AppUser class

---
+++ Code/src/Models/User.cs
@@ -0,0 +1,25 @@
+﻿namespace BettingSystem.Models
+{
+    //represents a user
+    public class AppUser
+    {
+        public int UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Dob { get; set; }
+        public string Email { get; set; }
+        public decimal WalletBalance { get; set; }
+        public string Role { get; set; }
+
+        public AppUser(int id, string fName, string lName, DateTime DOB, string mail, decimal balance, string userRole  )
+        {
+            UserID = id;
+            FirstName = fName;
+            LastName = lName;
+            Dob = DOB;
+            Email = mail;
+            WalletBalance = balance;
+            Role = userRole;
+        }
+    }
+}
