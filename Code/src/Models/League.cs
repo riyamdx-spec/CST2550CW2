6f5b514b4f6e1b75afbdb1be029117e51c0c7a64
Megane Coret <m01014266.mdx@gmail.com>
2026-03-02 00:31:38 +0400

    Add League class to represent leagues

---
+++ Code/src/Models/League.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BettingSystem.Models
+{
+    public class League
+    {
+        public int LeagueId { get; set; }
+        public string Name { get; set; }
+        public string LogoPath { get; set; }
+
+        public League(int id, string LeagueName, string Path)
+        {
+            LeagueId = id;
+            Name = LeagueName;
+            LogoPath = Path;
+        }
+    }
+}
