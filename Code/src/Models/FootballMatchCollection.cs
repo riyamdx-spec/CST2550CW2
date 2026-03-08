3788f36a6b36c0b99bd45646834b90e109b13d65
Megane Coret <m01014266.mdx@gmail.com>
2026-03-08 13:52:19 +0400

    Add FootballMatchCollection to store all matches and group them by league

---
+++ Code/src/Models/FootballMatchCollection.cs
@@ -0,0 +1,9 @@
+﻿namespace BettingSystem.Models
+{
+    // collection of football matches
+    public record FootballMatchCollection
+    (
+        SortedSet<FootballMatch> AllMatches, // store all matches
+        Dictionary<int, SortedSet<FootballMatch>> MatchesByLeague // matches keyed by league id
+    );
+}
