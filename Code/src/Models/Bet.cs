b001bfff3a646788ce07123e047bae6a2c0d9321
GwinYaki <lioshymellow@gmail.com>
2026-03-13 01:34:06 +0400

    Add Bet model for in-memory bet slip

---
+++ Code/src/Models/Bet.cs
@@ -0,0 +1,24 @@
+﻿namespace BettingSystem.Models
+{
+    public class Bet
+    {
+        public int OddID { get; set; }
+        public string Selection { get; set; }
+        public decimal OddValue { get; set; }
+        public int GameID { get; set; }
+        public int BetTypeID { get; set; }
+
+        // store node reference 
+        public LinkedListNode<Bet>? Node { get; set; }
+
+        public Bet(int oddID, string selection, decimal oddValue, int betTypeID, int gameID)
+        {
+            OddID = oddID;
+            Selection = selection;
+            OddValue = oddValue;
+            GameID = gameID;
+            BetTypeID = betTypeID;
+        }
+    }
+}
+
