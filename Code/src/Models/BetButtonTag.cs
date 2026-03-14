068139b2492f703615a652ac966ac37f0438beb1
Megane Coret <m01014266.mdx@gmail.com>
2026-03-14 15:24:21 +0400

    Add BetButtonTag record to store BetTypeId and Selection for bet buttons tags

---
+++ Code/src/Models/BetButtonTag.cs
@@ -0,0 +1,9 @@
+﻿namespace BettingSystem.Models
+{
+    //info to store in the tag of a bet button
+    public record BetButtonTag
+    (
+        int BetTypeId,
+        string Selection
+    );
+}
