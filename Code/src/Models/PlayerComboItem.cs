c22d89f4dd46ffd50420b49dd377017bb9fabd21
Megane Coret <m01014266.mdx@gmail.com>
2026-03-14 16:15:10 +0400

    Add PlayerComboItem class for player ComboBox in Main Page

---
+++ Code/src/Models/PlayerComboItem.cs
@@ -0,0 +1,22 @@
+﻿namespace BettingSystem.Models
+{
+    //represent player to be displayed in comboBox on main page
+    public class PlayerComboItem
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public Odd PlayerOdd { get; set; }
+
+        public PlayerComboItem(string name, string position, Odd playerOdd)
+        {
+            Name = name;
+            Position = position;
+            PlayerOdd = playerOdd;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Position} - Odd: {PlayerOdd.OddValue}";
+        }
+    }
+}
