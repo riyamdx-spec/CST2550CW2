b25b2fece9c3ef2d84406c6cdc4f6941e2fb96cb
Megane Coret <m01014266.mdx@gmail.com>
2026-03-14 16:28:28 +0400

    Update Properties Folder

--- Code/src/Forms/Properties/Resources.Designer.cs
+++ Code/src/Forms/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace BettingSystem.Forms.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        public static System.Drawing.Bitmap add {
+            get {
+                object obj = ResourceManager.GetObject("add", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         public static System.Drawing.Bitmap backBtn {
             get {
                 object obj = ResourceManager.GetObject("backBtn", resourceCulture);
