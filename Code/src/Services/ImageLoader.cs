0c2a28495e25923e651d1b85d4ba596b6922f809
Megane Coret <m01014266.mdx@gmail.com>
2026-03-14 15:17:12 +0400

    Add ImageLoader class to load images from files or URLs

---
+++ Code/src/Services/ImageLoader.cs
@@ -0,0 +1,28 @@
+﻿namespace BettingSystem.Services
+{
+    public class ImageLoader
+    {
+        private static readonly HttpClient client = new HttpClient();
+        public async Task<Image> GetImageAsync(string url)
+        {
+            try
+            {
+                //image from a folder
+                if (File.Exists(url))
+                {
+                    return Image.FromFile(url);
+                }
+                //image from web
+                using (Stream stream = await client.GetStreamAsync(url))
+                {
+                    return Image.FromStream(stream);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error loading image: {ex.Message}");
+                return null; 
+            }
+        }
+    }
+}
