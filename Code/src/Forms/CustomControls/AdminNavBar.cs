7d178d36230453e5281f4d22063ebcfacb895e4c
Megane Coret <m01014266.mdx@gmail.com>
2026-03-17 23:39:23 +0400

    Add User Control AdminNavBar for the nav bar in admin panel

---
+++ Code/src/Forms/CustomControls/AdminNavBar.cs
@@ -0,0 +1,73 @@
+﻿using BettingSystem.Models;
+
+namespace BettingSystem.Forms.CustomControls
+{
+    public partial class AdminNavBar : UserControl
+    {
+        private AppUser ConnectedAdmin;
+
+        //click events
+        public event EventHandler UsersPageClicked;
+        public event EventHandler SearchMatchesPageClicked;
+        public event EventHandler FinancialPageClicked;
+        public AdminNavBar()
+        {
+            InitializeComponent();
+        }
+
+        public void SetAdmin(AppUser loggedInUser)
+        {
+            ConnectedAdmin = loggedInUser;
+            DisplayInfo();
+        }
+
+        public void DisplayInfo()
+        {
+            navDropdownBtn.Text = $"{ConnectedAdmin.FirstName} {ConnectedAdmin.LastName}";
+        }
+
+        // to open dropwdown menu
+        private void navDropdownBtn_Click(object sender, EventArgs e)
+        {
+            dropdownList.Show(navDropdownBtn, 0, navDropdownBtn.Height + 10);
+            dropdownList.Width = navDropdownBtn.Width;
+            editProfileToolStripMenuItem.Width = navDropdownBtn.Width;
+            changePasswordToolStripMenuItem.Width = navDropdownBtn.Width;
+            logOutToolStripMenuItem.Width = navDropdownBtn.Width;
+        }
+
+        private void ChangedPassword_Click(object sender, EventArgs e)
+        {
+            ChangePasswordPopup passwordPopup = new ChangePasswordPopup(ConnectedAdmin);
+            passwordPopup.ShowDialog();
+        }
+
+        private void EditProfile_Click(object sender, EventArgs e)
+        {
+            EditFormPopup editFormPopup = new EditFormPopup(ConnectedAdmin);
+            editFormPopup.ShowDialog();
+        }
+
+        private void LogOut_Click(object sender, EventArgs e)
+        {
+            ConnectedAdmin = null;
+
+            // go back to landing page
+        }
+
+        private void usersPageBtn_Click(object sender, EventArgs e)
+        {
+            UsersPageClicked?.Invoke(this, new EventArgs());
+        }
+
+        private void matchSearchPageBtn_Click(object sender, EventArgs e)
+        {
+            SearchMatchesPageClicked?.Invoke(this, new EventArgs());
+        }
+
+        private void financialPageBtn_Click(object sender, EventArgs e)
+        {
+            FinancialPageClicked?.Invoke(this, new EventArgs());
+        }
+    }
+}
