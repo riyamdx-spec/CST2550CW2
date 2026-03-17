be2d988d5a344aa04ced07768da83e12da3c4bd1
Megane Coret <m01014266.mdx@gmail.com>
2026-03-17 15:41:53 +0400

    Add GameResult class to represent result of a football match

---
+++ Code/src/Models/GameResult.cs
@@ -0,0 +1,25 @@
+﻿namespace BettingSystem.Models
+{
+    public class GameResult
+    {
+        public int GameId { get; set; }
+        public int HomeTeamScore { get; set; }
+        public int AwayTeamScore { get; set; }
+        public int TotalCorners { get; set; }
+        public int RedCards { get; set; }
+        public int YellowCards { get; set; }
+
+        public string? FirstScorerName { get; set; }
+
+        public GameResult(int gameId, int homeTeamScore, int awayTeamScore, int totalCorners, int redCards, int yellowCards, string? firstScorerName)
+        {
+            GameId = gameId;
+            HomeTeamScore = homeTeamScore;
+            AwayTeamScore = awayTeamScore;
+            TotalCorners = totalCorners;
+            RedCards = redCards;
+            YellowCards = yellowCards;
+            FirstScorerName = firstScorerName;
+        }
+    }
+}
