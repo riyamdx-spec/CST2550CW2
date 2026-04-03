0f4880a6eaeb8713a3dcdf1e5d23f549eb9fbfc6
GwinYaki <lioshymellow@gmail.com>
2026-04-03 04:02:11 +0400

    Add date parameter to Bet model

--- Code/src/Models/Bet.cs
+++ Code/src/Models/Bet.cs
@@ -9,17 +9,19 @@ namespace BettingSystem.Models
         public decimal OddValue { get; set; }
         public int GameID { get; set; }
         public int BetTypeID { get; set; }
+        public DateTime Date { get; set; }
 
         // store node reference 
         public MyLinkedList<Bet>.Node? Node { get; set; }
 
-        public Bet(int oddID, string selection, decimal oddValue, int betTypeID, int gameID)
+        public Bet(int oddID, string selection, decimal oddValue, int betTypeID, int gameID, DateTime date)
         {
             OddID = oddID;
             Selection = selection;
             OddValue = oddValue;
             GameID = gameID;
             BetTypeID = betTypeID;
+            Date = date;
         }
     }
 }
